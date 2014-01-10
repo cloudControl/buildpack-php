6cb3c75fca31100b4df997656a8a3bb3c54ab15d
Dimitris Verraros <vervas.g@gmail.com>
2014-01-10 15:44:38 +0100

    Minor cleanup

--- support/merge_conf.php
+++ support/merge_conf.php
@@ -1,15 +1,13 @@
 <?php
 
-$files = $argv;
-
-if (count($files) != 4){
+if (count($argv) != 4){
     throw new Exception(
         sprintf('Invalid arguments passed')
 );
 }
 
-$default_php_fpm_ini = parse_ini_file($files[1], true, INI_SCANNER_RAW);
-$custom_php_fpm_ini = parse_ini_file($files[2], true, INI_SCANNER_RAW);
+$default_php_fpm_ini = parse_ini_file($argv[1], true, INI_SCANNER_RAW);
+$custom_php_fpm_ini = parse_ini_file($argv[2], true, INI_SCANNER_RAW);
 
 $configuration = array_replace_recursive($default_php_fpm_ini, $custom_php_fpm_ini);
 
@@ -19,8 +17,7 @@ $globals = '';
 if (!empty($configuration)) {
     foreach ($configuration as $section => $item) {
         if (!is_array($item)) {
-            $value = $item;
-            $globals .= $section . ' = ' . $value . "\n";
+            $globals .= $section . ' = ' . $item . "\n";
         }
     }
     $content .= $globals;
@@ -31,13 +28,11 @@ if (!empty($configuration)) {
             foreach ($item as $key => $value) {
                 if (is_array($value)) {
                     foreach ($value as $arrkey => $arrvalue) {
-                        //$arrvalue = $this->normalizeValue($arrvalue);
                         $arrkey = $key . '[' . $arrkey . ']';
                         $sections .= $arrkey . ' = ' . $arrvalue
                                     . "\n";
                     }
                 } else {
-                    //$value = $this->normalizeValue($value);
                     $sections .= $key . ' = ' . $value . "\n";
                 }
             }
@@ -46,7 +41,7 @@ if (!empty($configuration)) {
     $content .= $sections;
 }
 
-$filename = $files[3];
+$filename = $argv[3];
 
 if (false === file_put_contents($filename, $content)) {
     throw new Exception(
