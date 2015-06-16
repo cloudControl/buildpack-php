300b470c83f1f8497c85de53610149fdec7614f1
Tobias Wilken <tooangel@tooangel.de>
2015-06-16 10:45:15 +0200

    Update newrelic agent
    Use the right newrelic library for the different php versions

--- bin/util/newrelic.sh
+++ bin/util/newrelic.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
 function install_newrelic() {
-  NEWRELIC_VERSION="newrelic-php5-4.20.2.95-linux"
+  NEWRELIC_VERSION="4.22.0.99"
+  NEWRELIC_VERSION_FULL=newrelic-php5-${NEWRELIC_VERSION}-linux
   NEWRELIC_EXTENSION="newrelic-20100525.so"
   if [[ $PHP_VERSION == php-5.5.* ]]; then
     NEWRELIC_EXTENSION="newrelic-20121212.so"
+  elif [[ $PHP_VERSION == php-5.6.* ]]; then
+    NEWRELIC_EXTENSION="newrelic-20131226.so"
   fi
 
-  curl --silent --location https://download.newrelic.com/php_agent/release/${NEWRELIC_VERSION}.tar.gz  | tar zx
-  cp ${NEWRELIC_VERSION}/daemon/newrelic-daemon.x64 /srv/www/${PHP_VERSION}/bin
-  cp ${NEWRELIC_VERSION}/agent/x64/${NEWRELIC_EXTENSION} /srv/www/${PHP_VERSION}/lib/php/extensions/newrelic.so
-  rm -r ${NEWRELIC_VERSION}
+  curl --silent --location https://download.newrelic.com/php_agent/archive/${NEWRELIC_VERSION}/${NEWRELIC_VERSION_FULL}.tar.gz  | tar zx
+  cp ${NEWRELIC_VERSION_FULL}/daemon/newrelic-daemon.x64 /srv/www/${PHP_VERSION}/bin
+  cp ${NEWRELIC_VERSION_FULL}/agent/x64/${NEWRELIC_EXTENSION} /srv/www/${PHP_VERSION}/lib/php/extensions/newrelic.so
+  rm -r ${NEWRELIC_VERSION_FULL}
 
   echo "newrelic.logfile = /dev/null" >> ${PHP_CONFIGS_PATH}/newrelic.ini
 
@@ -21,8 +24,11 @@ function install_newrelic() {
 
 function finalize_newrelic() {
   echo "newrelic.logfile = stdout" > ${PHP_CONFIGS_PATH}/newrelic.ini
+  echo "newrelic.loglevel = info" > ${PHP_CONFIGS_PATH}/newrelic.ini
+  echo "newrelic.daemon.dont_launch = 0" > ${PHP_CONFIGS_PATH}/newrelic.ini
   echo "newrelic.daemon.location = /srv/www/${PHP_VERSION}/bin/newrelic-daemon.x64" >> ${PHP_CONFIGS_PATH}/newrelic.ini
   echo "newrelic.daemon.logfile = stdout" >> ${PHP_CONFIGS_PATH}/newrelic.ini
-  echo "newrelic.daemon.loglevel = error" >> ${PHP_CONFIGS_PATH}/newrelic.ini
-  echo "newrelic.daemon.pidfile = ${TMPDIR}/newrelic-daemon.pid" >> ${PHP_CONFIGS_PATH}/newrelic.ini
+  echo "newrelic.daemon.loglevel = info" >> ${PHP_CONFIGS_PATH}/newrelic.ini
+  echo "newrelic.daemon.pidfile = /app/newrelic-daemon.pid" >> ${PHP_CONFIGS_PATH}/newrelic.ini
+  echo "newrelic.daemon.port = /app/newrelic.sock" >> ${PHP_CONFIGS_PATH}/newrelic.ini
 }
